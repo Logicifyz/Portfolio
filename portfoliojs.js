2f6ad7ae9dca2f429c3feb64c2d28e8b83b267f7
Zhi Yang <zhiyang111704@gmail.com>
2023-07-10 14:45:35 +0800

    initial commit

---
+++ portfoliojs.js
@@ -0,0 +1,100 @@
+/*
+Name: Wee Zhi Yang
+Admin No. : 220890R
+Class: IT2301 
+Website: Porfolio (Name??)
+*/
+
+/*Clock function */
+function clock() {// We create a new Date object and assign it to a variable called "time".
+    var time = new Date(),
+        
+        // Access the "getHours" method on the Date object with the dot accessor.
+        hours = time.getHours(),
+        
+        // Access the "getMinutes" method with the dot accessor.
+        minutes = time.getMinutes(),
+        
+        
+        seconds = time.getSeconds();
+    
+    document.querySelectorAll('.clock')[0].innerHTML = harold(hours) + " : " + harold(minutes) + " : " + harold(seconds);
+      
+      function harold(standIn) {
+        if (standIn < 10) {
+          standIn = '0' + standIn
+        }
+        return standIn;
+      }
+    }
+    setInterval(clock, 1000);
+    /*End of clock function */
+
+/*Sliding image track */
+const track = document.getElementById("image-track");
+
+const handleOnDown = e => track.dataset.mouseDownAt = e.clientX;
+
+const handleOnUp = () => {
+  track.dataset.mouseDownAt = "0";  
+  track.dataset.prevPercentage = track.dataset.percentage;
+}
+
+const handleOnMove = e => {
+  if(track.dataset.mouseDownAt === "0") return;
+  
+  const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
+        maxDelta = window.innerWidth / 2;
+  
+  const percentage = (mouseDelta / maxDelta) * -100,
+        nextPercentageUnconstrained = parseFloat(track.dataset.prevPercentage) + percentage,
+        nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
+  
+  track.dataset.percentage = nextPercentage;
+  
+  track.animate({
+    transform: `translate(${nextPercentage}%, -50%)`
+  }, { duration: 1200, fill: "forwards" });
+  
+  for(const image of track.getElementsByClassName("image")) {
+    image.animate({
+      objectPosition: `${100 + nextPercentage}% center`
+    }, { duration: 1200, fill: "forwards" });
+  }
+}
+
+/* -- Had to add extra lines for touch events -- */
+
+window.onmousedown = e => handleOnDown(e);
+
+window.ontouchstart = e => handleOnDown(e.touches[0]);
+
+window.onmouseup = e => handleOnUp(e);
+
+window.ontouchend = e => handleOnUp(e.touches[0]);
+
+window.onmousemove = e => handleOnMove(e);
+
+window.ontouchmove = e => handleOnMove(e.touches[0]);
+
+/*End of sliding image track */
+
+/* Scroll Animator*/
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry) 
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    } else {
+      entry.target.classList.remove('show');
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((eL) => { observer.observe(eL) });
+/* End of scroll animator */
+
+
+
